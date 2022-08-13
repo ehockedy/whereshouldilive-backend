eb6b48610f6488a5ff644c48a6383c878365c828
ehockedy <edhockedy@hotmail.com>
2022-08-13 14:52:08 +0100

    Updated API definitions

--- app/src/main/java/wsil/RankPlacesToLiveApiController.java
+++ app/src/main/java/wsil/RankPlacesToLiveApiController.java
@@ -1,29 +1,29 @@
 package wsil;
 
+import java.util.List;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.api.RankPlacesToLiveApi;
 import io.swagger.model.PlaceRankSummaries;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Schema;
-
 
 @RestController
 public class RankPlacesToLiveApiController implements RankPlacesToLiveApi {
     private static final Logger log = LoggerFactory.getLogger(RankPlacesToLiveApiController.class);
 
+    @Override
     public ResponseEntity<PlaceRankSummaries> rankPlacesToLive(
-      @Parameter(in = ParameterIn.QUERY, description = "How many items to return at one time (max 100)", schema=@Schema()) @Valid @RequestParam(value = "limit", required = false) Integer limit) {
-        log.info("Limit:" + limit);
+        @NotNull @Valid List<String> placesToLive,
+        @NotNull @Valid List<String> importantPlaces) {
+            log.info("ptl: " + placesToLive.size());
+            log.info("ip: " + importantPlaces.size());
         return new ResponseEntity<PlaceRankSummaries>(HttpStatus.NOT_IMPLEMENTED);
     }
-
 }
