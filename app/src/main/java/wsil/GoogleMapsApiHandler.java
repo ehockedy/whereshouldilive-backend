3833b46d8e91b5a3ba60d122bb9e86f8df486bd3
ehockedy <edhockedy@hotmail.com>
2022-08-23 20:58:12 +0100

    Added logic to ranking function
    It now calculates spoofed travel time permonth for each place, then
    ranks the list and returns it.
    
    Also made use of the google maps api.

--- app/src/main/java/wsil/GoogleMapsApiHandler.java
+++ app/src/main/java/wsil/GoogleMapsApiHandler.java
@@ -1,80 +1,25 @@
 package wsil;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Logger;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
 
 public class GoogleMapsApiHandler {
-	private static String apiKey;
-	private static HttpRequest.Builder httpRequestBuilder;
-	private static String googleMapsURL = "https://maps.googleapis.com/maps/api/";
-	private static Logger logger = Logger.getLogger(GoogleMapsApiHandler.class.getName());
+	private static GeoApiContext context;
 
 	public GoogleMapsApiHandler(String key) {
-		apiKey = key;
-		httpRequestBuilder = HttpRequest.newBuilder();
+		context = new GeoApiContext.Builder().apiKey(key).build();
 	}
 
-	String getPlaceIdFromAddress(String address) throws IOException, InterruptedException {
-		// Make a request to google geocode API to get all the info on the address
-		// The address should be enough to get information
-		String sanitizedAddress = address.replace(" ", "-");
-		HttpRequest request = httpRequestBuilder
-				.uri(URI.create(googleMapsURL + "geocode/json?address=" + sanitizedAddress + "&key=" + apiKey))
-				.method("GET", HttpRequest.BodyPublishers.noBody())
-				.build();
-
-		// Parse the response to get the place ID
-		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-		JSONParser parser = new JSONParser();
-		String placeID = "";
-		try {
-			JSONObject obj = (JSONObject) (parser.parse(response.body()));
-			JSONArray resultsArray = (JSONArray) (obj.get("results"));
-			if (resultsArray.size() > 1) {
-				logger.warning("More than one response for place with address " + sanitizedAddress);
-			} else if (resultsArray.size() == 0) {
-				logger.warning("No place found for " + sanitizedAddress);
-			} else {
-				JSONObject firstResult = (JSONObject) resultsArray.get(0);
-				placeID = (String) firstResult.get("place_id");
-			}
-
-		} catch (ParseException pe) {
-			logger.severe(pe.getMessage());
-		}
-		return placeID;
-	}
-
-	JSONObject mapsMatrixRequest(List<String> placesToTry, List<String> selectedPlaces) {
-		String origins = "place_id:" + String.join("%7Cplace_id:", placesToTry);
-		String destinations = "place_id:" + String.join("%7Cplace_id:", selectedPlaces);
-		HttpRequest request = httpRequestBuilder
-			.uri(URI.create(googleMapsURL + "distancematrix/json?key=" + apiKey + "&origins=" + origins + "&destinations=" + destinations))
-			.method("GET", HttpRequest.BodyPublishers.noBody())
-			.build();
-
-		HttpResponse<String> response;
-		JSONParser parser = new JSONParser();
-		try {
-			response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-			return (JSONObject) (parser.parse(response.body()));
-		} catch (IOException | InterruptedException | ParseException e) {
-			e.printStackTrace();
-		}
-
-		HashMap<String, String> errorResponse = new HashMap<String, String>();
-		errorResponse.put("status", "error");
-		return new JSONObject(errorResponse);
+	// Synchronously make Google maps distance matrix API call
+	DistanceMatrix mapsMatrixRequest(List<String> placesToTry, List<String> selectedPlaces) throws ApiException, InterruptedException, IOException {
+		return new DistanceMatrixApiRequest(context)
+			.origins(placesToTry.toArray(new String[placesToTry.size()]))
+			.destinations(selectedPlaces.toArray(new String[selectedPlaces.size()]))
+			.await();
 	}
 }
--- app/src/main/java/wsil/RankPlacesToLiveApiController.java
+++ app/src/main/java/wsil/RankPlacesToLiveApiController.java
@@ -1,6 +1,11 @@
 package wsil;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
@@ -13,8 +18,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixElementStatus;
+import com.google.maps.model.DistanceMatrixRow;
+
 import io.swagger.api.RankPlacesToLiveApi;
 import io.swagger.model.PlaceRankSummaries;
+import io.swagger.model.PlaceRankSummary;
 
 @RestController
 public class RankPlacesToLiveApiController implements RankPlacesToLiveApi {
@@ -33,10 +45,59 @@ public class RankPlacesToLiveApiController implements RankPlacesToLiveApi {
     public ResponseEntity<PlaceRankSummaries> rankPlacesToLive(
         @NotNull @Valid List<String> placesToLive,
         @NotNull @Valid List<String> importantPlaces) {
-            log.info("ptl: " + placesToLive.size());
-            log.info("ip: " + importantPlaces.size());
-            log.info("apiKey: " + apiKey);
 
-        return new ResponseEntity<PlaceRankSummaries>(HttpStatus.NOT_IMPLEMENTED);
+        // Make distance matrix request
+        DistanceMatrix mapMatrixResponse;
+        try {
+            mapMatrixResponse = this.googleMapsApiHandler.mapsMatrixRequest(placesToLive, importantPlaces);
+        } catch (ApiException e) {
+            e.printStackTrace();
+            return new ResponseEntity<PlaceRankSummaries>(HttpStatus.BAD_REQUEST);
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<PlaceRankSummaries>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        // Process results
+        // Every row corresponds to a potential place to live. For each one, calulate the total time travelling
+        // per month. Then rank the places to live.
+        PlaceRankSummaries placeRankSummaries = new PlaceRankSummaries();
+        List<Long> travelTimesPerMonth = new ArrayList<>();  // TODO long implies once per month is least frequent
+        for (int i = 0; i < importantPlaces.size(); i++) {
+            travelTimesPerMonth.add(1L);
+        }
+        
+        ListIterator<String> it = placesToLive.listIterator();
+        while (it.hasNext()) {
+            int idx = it.nextIndex();
+            String placeName = it.next();
+            Optional<Long> ttpm = calculateTravelTimePerMonth(mapMatrixResponse.rows[idx], travelTimesPerMonth);
+            placeRankSummaries.add(
+                new PlaceRankSummary()
+                    .name(placeName)
+                    .success(ttpm.isPresent())
+                    .totalTravelTimePerMonth(ttpm.orElse(0L))
+            );
+        }
+
+        // Sort results from shortest to longest travel time per month
+        placeRankSummaries.sort(Comparator.comparingLong(PlaceRankSummary::getTotalTravelTimePerMonth));
+
+        return new ResponseEntity<PlaceRankSummaries>(placeRankSummaries, HttpStatus.OK);
+    }
+
+    Optional<Long> calculateTravelTimePerMonth(DistanceMatrixRow row, List<Long> travelTimesPerMonth) {
+        Long total = 0L;
+        int idx = 0;
+        for (DistanceMatrixElement element : row.elements) {
+            if (element.status == DistanceMatrixElementStatus.OK) {
+                total += element.duration.inSeconds * travelTimesPerMonth.get(idx);
+            } else {
+                // Distance calculation could not be caluclated for one of the places, so evaluation
+                // of this place to live is invalid.
+                return Optional.empty();
+            }
+        }
+        return Optional.of(total);
     }
 }
